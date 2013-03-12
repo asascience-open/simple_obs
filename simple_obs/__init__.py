c4040c314b34084cc22da726382960df86005395
Kyle Wilcox <kwilcox@asascience.com>
2013-03-12 14:49:50 -0400

    Have log path respect the SIMPLEOBS_PATH env variable (for production)

--- simple_obs/__init__.py
+++ simple_obs/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,7 +9,8 @@ app.config.from_object('simple_obs.config')
 if app.config.get('LOG_FILE') == True:
     import logging
     from logging import FileHandler
-    file_handler = FileHandler('logs/simple_obs.txt')
+    p = os.path.join(os.environ.get("SIMPLEOBS_PATH",""),"logs","simple_obs.txt")
+    file_handler = FileHandler(p)
     file_handler.setLevel(logging.DEBUG)
     app.logger.addHandler(file_handler)
 
